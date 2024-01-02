5a673fbfad644e7602ed03a23145cff95e334bc9
G0nz0pera <155404796+g0nz0pera@users.noreply.github.com>
2024-01-02 01:15:23 -0300

    initial commit

---
+++ src/main/java/com/g0nz0/users/ws/UsersMobileWsApplication.java
@@ -0,0 +1,13 @@
+package com.g0nz0.users.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersMobileWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersMobileWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/g0nz0/users/ws/UsersMobileWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g0nz0.users.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersMobileWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
